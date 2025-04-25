f6bfd91fb48c4d866e3cbcfcec9e938cc82afe1b
vkvijeth18 <vkvijethkumar24@gmail.com>
2025-04-25 15:17:05 +0530

    updated cookie

--- backend/utils/generateTokenNsetCookie.js
+++ backend/utils/generateTokenNsetCookie.js
@@ -7,6 +7,7 @@ const generateTokenAndSetCookies = async (userId, res) => {
   res.cookie("jwt_token_UpSkillMe", token, {
     maxAge: 15 * 24 * 60 * 60 * 1000,
     httpOnly: true,
+    sameSite: "None",
     secure: process.env.NODE_ENV !== "development",
   });
 };
